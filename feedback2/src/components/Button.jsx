2a1426657cde4d8fc5bbf7dd0baa26e177944b3e
melvin <ianndiormel@gmail.com>
2025-01-18 23:43:12 +0100

    added the toggle function

--- feedback2/src/components/Button.jsx
+++ feedback2/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
-export default function Button () {
+export default function Button ({setReverse, reverse}) {
+    const handleToggle = () => {
+        setReverse(!reverse)
+    }
     const [toggled, setToggled] = useState(false);
     return (
-        <button className={`toggle-btn ${toggled ? 'toggled' : ''}`} onClick={() => setToggled(!toggled)}>
+        <button className={`toggle-btn ${toggled ? 'toggled' : ''}`} onClick={() => setToggled(!toggled)} onClickCapture={handleToggle}>
         <div className="thumb"></div>
       </button>
     )
--- feedback2/src/components/FeedbackItem.jsx
+++ feedback2/src/components/FeedbackItem.jsx
@@ -1,12 +1,15 @@
 import Card from "./Shared/Card";
 import Button from './Button';
+import { useState } from "react";
+
 const FeedbackItem = ({ item }) => {
+  const [reverse, setReverse] = useState(false)
   return (
-    <Card reverse= {true}>
+    <Card reverse= {reverse}>
       <div className="num-display">{item.rating}</div>
 
       <div className="text-display"> {item.text}</div>
-      <Button />
+      <Button setReverse={setReverse} reverse={reverse}/>
       
     </Card>
   );
